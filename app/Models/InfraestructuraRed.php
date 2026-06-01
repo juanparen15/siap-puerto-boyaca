8bb65fad4dc8bab1004e43e2c60351ed0cfc75c0
juanparen15 <jprendon9@gmail.com>
2026-06-01 11:17:40 -0500

    fix: add $table = 'infraestructura_redes' to InfraestructuraRed model
    Laravel pluralizes InfraestructuraRed to infraestructura_reds but
    the migration created the table as infraestructura_redes.

--- app/Models/InfraestructuraRed.php
+++ app/Models/InfraestructuraRed.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class InfraestructuraRed extends Model
 {
+    protected $table = 'infraestructura_redes';
+
     protected $fillable = [
         'nombre', 'tipo', 'uso', 'clasificacion', 'material',
         'calibre_conductores', 'tipo_instalacion', 'tipo_zona',
